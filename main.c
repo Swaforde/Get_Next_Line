93c79b6eca51f797e2569e51487206bb47186bde
Theo Bouverat <tbouvera@student.42lausanne.ch>
2022-10-25 11:05:53 +0200

    ok

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tbouvera <tbouvera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 13:02:55 by tbouvera          #+#    #+#             */
-/*   Updated: 2022/10/21 15:03:30 by tbouvera         ###   ########.fr       */
+/*   Updated: 2022/10/25 09:41:31 by tbouvera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ int	main(void)
 {
 	int	fd;
 	fd = open("text.txt", O_RDONLY);
-	printf ("%s", get_next_line(fd));
 	printf ("%s", get_next_line(fd));
 	return (1);
 }
