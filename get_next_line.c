710a0aec452e178afeba71ecde4ad85014c79af2
Theo Bouverat <tbouvera@student.42lausanne.ch>
2022-10-26 12:03:47 +0200

    fixe gros leaks

--- get_next_line.c
+++ get_next_line.c
@@ -1,6 +1,15 @@
 #include "get_next_line.h"
 //#define BUFFER_SIZE 10
 
+void	ft_free(char **p)
+{
+    if (p && *p)
+    {
+        free(*p);
+        *p = NULL;
+    }
+}
+
 char	*ft_get_first_line(char *buffer)
 {
 	char	*ptr;
@@ -8,19 +17,18 @@ char	*ft_get_first_line(char *buffer)
 	
 	i = 0;
 	if (buffer[i] == '\0')
-		return (NULL);a
+		return (NULL);
 	while (buffer[i] != '\0' && buffer[i] != '\n')
 		i++;
-	ptr	
-	return (NULL);
+	ptr = malloc (sizeof(char) * (i) + 2);
+	if (!ptr)
+		return (NULL);
 	i = 0;
 	while (buffer[i] != '\0' && buffer[i] != '\n')
 	{
 		ptr[i] = buffer[i];
 		i ++;
-	} = malloc (sizeof(char) * (i) + 2);
-	if (!ptr)
-	
+	}
 	if (buffer[i] == '\n')
 	{
 		ptr[i] = '\n';
@@ -30,7 +38,7 @@ char	*ft_get_first_line(char *buffer)
 	return (ptr);
 }
 
-char	*save_read(int fd, char *buffer, int *check)
+char	*save_read(int fd, char *buffer)
 {
 	char	*buff;
 	char	*tmp;
@@ -50,23 +58,24 @@ char	*save_read(int fd, char *buffer, int *check)
 		if (read_bytes == -1)
 		{
 			free(buff);
-			free (tmp);
+			free(tmp);
 			return (NULL);
 		}
 		if (read_bytes == 0)
 		{
 			free(buff);
 			free(tmp);
-			*check = 1;
 			return (buffer);
 		}
 		buff[read_bytes] = '\0';
+		if (tmp != NULL)
+			free (tmp);
 		tmp = ft_strdup(buffer);
 		free(buffer);
 		buffer = ft_strjoin(tmp, buff);
 	}
 	free(buff);
-	free (tmp);
+	free(tmp);
 	return (buffer);
 }
 
@@ -104,40 +113,30 @@ char	*ft_save(char *buffer)
 		t ++;
 	}
 	ptr[s] = '\0';
-	free(buffer);
 	return (ptr);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*buffer;
-
 	char		*line;
-	char		*test;
-	int			checker;
-
-	checker = 0;
+	char		*tmp;
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
-	test = malloc (sizeof(char) * BUFFER_SIZE + 1);
-	if (!test)
-		return (NULL);
-	ft_bzero(test, BUFFER_SIZE + 1);
 	if (buffer == NULL)
 	{
 		buffer = malloc (sizeof(char) * 1);
-		buffer[0] = '\0';
 		if (!buffer)
-		{
-			free (test);
 			return (NULL);
-		}
+		buffer[0] = '\0';
 	}
-	buffer = save_read(fd, buffer, &checker);
+	buffer = save_read(fd, buffer);
+	if (buffer == NULL)
+		return (NULL);
+	tmp = ft_strdup(buffer);
 	line = ft_get_first_line(buffer);
-	buffer = ft_save(buffer);
-	free (test);
-	if (checker == 1)
-		free (buffer);
+	free (buffer);
+	buffer = ft_save(tmp);
+	free (tmp);
 	return (line);
 }
