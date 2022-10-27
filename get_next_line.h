72973ce3976b0292026cf25f6011cc2c7a348f57
Théo Bouverat <theo@MacBook-Pro-de-Theo.local>
2022-10-27 10:38:20 +0200

    header

--- get_next_line.h
+++ get_next_line.h
@@ -22,5 +22,4 @@ size_t	ft_strlen(const char *str);
 char	*ft_strdup(const char *s1);
 char	*ft_strjoin(char const *s1, char const *s2);
 int		ft_check_error(int fd, int buff_size, char **buffer);
-void	*ft_calloc(size_t count, size_t size);
 #endif
